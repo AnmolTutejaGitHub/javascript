ec5a4279d1aeb576197c928e74f5a42b9424ec1e
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-16 15:57:39 +0530

    Update type conversion and type coercion.js

--- javascript basics/type conversion and type coercion.js
+++ javascript basics/type conversion and type coercion.js
@@ -12,6 +12,7 @@ console.log(inputYear+18);
 console.log(Number("Jonas"));
 console.log(typeof NaN);
 
+console.log(String(23));
 //   //  ////////   //////
 //   /    /////    /////
 //   /     /////    /////
