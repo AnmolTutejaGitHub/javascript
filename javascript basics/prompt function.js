01895fcb6039c7d01a545bfc2c901e1b3b954149
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-16 23:08:53 +0530

    Add files via upload

---
+++ javascript basics/prompt function.js
@@ -0,0 +1,31 @@
+// prompt function 
+
+const favourite=prompt("What's your favourite number?");
+
+console.log(favourite)
+console.log(typeof favourite)
+
+if(favourite==23){
+    console.log("cool! 23 is amazing number!");
+}else{
+    console.log("Its Amazing!!")
+}
+
+
+
+
+const favourite1=Number(prompt("What's your favourite number?"));
+
+console.log(favourite1)
+console.log(typeof favourite1)
+
+if(favourite1===23){
+    console.log("cool! 23 is amazing number!");
+}else{
+    console.log("Its Amazing!!")
+}
+
+
+//not equal to operator
+// != lose version
+// !== strict version
