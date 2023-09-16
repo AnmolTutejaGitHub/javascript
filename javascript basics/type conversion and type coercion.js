e7d20bc3e484f4f27f83c480295c4f9b99562674
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-16 14:58:26 +0530

    Add files via upload

---
+++ javascript basics/type conversion and type coercion.js
@@ -0,0 +1,19 @@
+//Converting strings to a number
+let inputYear='1991';
+console.log(inputYear+18);
+
+console.log(Number(inputYear));
+console.log(inputYear+18);
+console.log(Number(inputYear)+18);
+
+inputYear=Number(inputYear);
+console.log(inputYear+18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+//   //  ////////   //////
+//   /    /////    /////
+//   /     /////    /////
+//  //   //////// /////// /////
+
