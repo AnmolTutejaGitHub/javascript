cbb70248bedb4537097b5f357a355b0fc59d2898
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-16 14:58:43 +0530

    Add files via upload

---
+++ javascript basics/type of operator.js
@@ -0,0 +1,36 @@
+//Type of operator
+
+let javascriptIsFun=true;
+
+console.log(typeof true);
+console.log(typeof 34);
+console.log(typeof "Anmol");
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+//BMI calculate que
+let massMark=78;
+let heightMark=1.69;
+let massJohn=92;
+let heightJohn=1.95;
+
+let BMIMark=(massMark)/(heightMark*heightMark);
+let BMIJohn=(massJohn)/(heightJohn*heightJohn);
+
+console.log(BMIMark,BMIJohn)
+
+
+
+
+
+
+
+
+
+
+
+
