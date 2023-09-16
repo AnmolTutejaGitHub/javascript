d3cd811e25bc93a4da12f86ec934e2d442e04868
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-16 22:32:15 +0530

    Add files via upload

---
+++ javascript basics/equality operator.js
@@ -0,0 +1,16 @@
+//Equality operator 
+
+// == is loose equality operator and  === is strict equality operator
+// means === doesn't perform type coercion
+
+if('18'==18){
+    console.log("yup")
+}else{
+    console.log("nah")
+}
+
+if('18'===18){
+    console.log("yup")
+}else{
+    console.log("nah")
+}
