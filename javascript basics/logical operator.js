e7f99b22f85355996ee83b3c23fe04f357c33c74
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-17 01:26:43 +0530

    Add files via upload

---
+++ javascript basics/logical operator.js
@@ -0,0 +1,13 @@
+// and operator &&
+// or operator ||
+// not operator !
+
+const hasDrivingLicence=true;
+const hasGoodVision=true;
+const isTired=false;
+
+if(hasDrivingLicence && hasDrivingLicence  && !isTired){
+ console.log("Can drive")
+}else{
+    console.log("can't  drive")
+}
