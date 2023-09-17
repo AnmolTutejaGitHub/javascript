9ffdd540c77b55d6ad5cf13e1f96e69f6a05ea63
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-17 13:21:15 +0530

    Add files via upload

---
+++ javascript basics/strict mode.js
@@ -0,0 +1,11 @@
+'use srtict'
+
+let hasDriversLicence=false;
+const passTest=true;
+
+if(passTest) hasDriverLicence=true; //intentially spelling mistake
+if(hasDriversLicence) console.log("I can drive")
+
+
+//strict mode prevents us from defining variables that can be keywords in future
+const interface='Audio';
