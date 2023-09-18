89fe71089441f6438903907b30f9f460b93f7ffa
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-18 12:39:56 +0530

    Add files via upload

---
+++ javascript basics/Objects in JavaScript/data structure objects.js
@@ -0,0 +1,21 @@
+//array 
+const jonasArray=['jonas',
+                 'Schmedtmann',
+                  2023-1991,
+                  'teacher',
+                   ['Michael','peter','steven']];
+
+
+//Object
+//key:value
+//keys also called properties
+//below is object literal syntax
+
+
+const jonas={
+            firstname:'jonas',
+            lastname:'Schmedtmann',
+            age :  2023-1991,
+            job: 'teacher',
+            friends:['Michael','peter','steven']
+};
