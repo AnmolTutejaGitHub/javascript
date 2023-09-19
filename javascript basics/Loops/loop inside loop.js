680912fe21250dfc43d899fa8c415bcdd899ebd8
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-19 17:43:57 +0530

    Add files via upload

---
+++ javascript basics/Loops/loop inside loop.js
@@ -0,0 +1,11 @@
+//loop inside loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------- Starting exercise ${exercise}`);
+  
+    for (let rep = 1; rep < 6; rep++) {
+      console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+    }
+  }
+
+  
