cedb0fe08f2ce7559dcc38cfa651d8fbb41107d6
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-19 20:45:27 +0530

    Add files via upload

---
+++ javascript basics/console.table in js.js
@@ -0,0 +1,9 @@
+//console.table will display object in tabular form in console 
+object_name={
+   name:'Anmol',
+   planet:'Earth',
+   indentity:'Humankind',
+   status:'Alive',
+   senses:"very human"
+};
+console.table(object_name);
