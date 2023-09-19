c2b146d37fd861e9918bb7b2304c77a581dca86e
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-09-19 17:29:57 +0530

    Add files via upload

---
+++ javascript basics/Loops/iterating through array.js
@@ -0,0 +1,21 @@
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+  ];
+  const types = [];
+  
+  for (let i = 0; i < jonas.length; i++) {
+    // Reading from jonas array
+    console.log(jonas[i], typeof jonas[i]);
+  
+    // Filling types array
+    // types[i] = typeof jonas[i]; or can fill array like this:
+    types.push(typeof jonas[i]);
+  }
+  
+  console.log(types);
