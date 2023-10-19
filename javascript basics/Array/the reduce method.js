869dfbc0503f56a75e5601ee88cecfad86a668d6
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-10-19 13:34:57 +0530

    Add files via upload

---
+++ javascript basics/Array/the reduce method.js
@@ -0,0 +1,28 @@
+//Reduce method 
+//we use the reduce method to essentially boil the elements in an array to a single value
+
+const movements=[200,450,-400,3000,-650,-130,70,1300]
+
+const balance=movements.reduce(function(acc,curr,i,arr){
+    return acc+curr;
+},0);
+console.log(balance) //3840
+//acc -- accumulator
+//curr -- current element
+//first argument of call back function is accumulator
+
+// 0 we specified there is the initial value of the accumulator
+// each loop iteration will return the updated acc 
+
+
+//using arrow as call back function
+const balance2=movements.reduce((acc,curr)=>acc+curr,0);
+console.log(balance2) //3840
+
+
+//for of loop sntax
+let balance3=0;  //extra variable
+for (const mov of movements) balance3+=mov;
+console.log(balance3) //3840
+
+
