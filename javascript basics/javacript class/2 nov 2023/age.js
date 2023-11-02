bfd1a471cfb5a75ad17e0df44e04ae7a73732db0
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-02 16:16:33 +0530

    Add files via upload

---
+++ javascript basics/javacript class/2 nov 2023/age.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program that takes a user's age as input and prompts them to enter their grade level. If the user's age is greater than or equal to 18, the program should print a message to the console saying that t
+// program that takes a user's age as input and prompts them to enter their grade level. If the user's age is greater than or equal to 18, the program should print a message to the console saying that the user is eligible to vote. If the user's age is less than 18, 
+//the program should print a message to the console saying that the user is not eligible to vote.
+
+//use console 
+let age = parseInt(prompt("Enter your age:"));
+if(age>=18) console.log("U are eligible");
+else console.log("You aren't");
---
+++ javascript basics/javacript class/max3.js
@@ -0,0 +1,12 @@
+let a=parseInt(prompt("Enter a:"));
+let b =parseInt(prompt("Enter b:"));;
+let c=parseInt(prompt("Enter c:"));;
+if(a>b && a>c){ 
+document.write("a is greatest"); 
+} 
+else if (b>a && b>c){ 
+document.write("b is greatest"); 
+} 
+else{
+    document.write("c is greatest");   
+}
