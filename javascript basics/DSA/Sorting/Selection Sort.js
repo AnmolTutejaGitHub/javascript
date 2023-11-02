72374cdfc4180c9feb836384dd78ccc8f36ffe2b
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-02 18:05:45 +0530

    Add files via upload

---
+++ javascript basics/DSA/Sorting/Selection Sort.js
@@ -0,0 +1,20 @@
+//Selection sort
+
+function selectionSort(arr){
+    for(var i=0;i<arr.length;i++){
+        var lowest=i;
+        for(var j=i+1;j<arr.length;j++){
+        if(arr[j]<arr[lowest]){
+            lowest=j;
+        }
+    }
+        if(i!==lowest){
+           // console.log(i,lowest)
+           var temp=arr[i];
+           arr[i]=arr[lowest];
+           arr[lowest]=temp
+        }
+    }
+    return arr;
+}
+console.log(selectionSort([0,2,34,22,10,17])) //[0, 2, 10, 17, 22, 34]
