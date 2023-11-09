c54db6ad3e0d4d315eb4be4290b3bbdfcbafcb3a
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-09 16:07:55 +0530

    Add files via upload

---
+++ javascript basics/void operator.js
@@ -0,0 +1,8 @@
+//Void operator:
+//The void operator can be used to discard the value of an expression. For example, the following code will print "undefined" to the console:
+
+//const x = null;
+const x=56;
+console.log(void x); // undefined
+
+
