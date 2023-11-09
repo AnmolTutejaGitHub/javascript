84c0521ac61974035851d3f69f16bf3a4668820a
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-09 15:57:02 +0530

    Add files via upload

---
+++ javascript basics/delete keyword.js
@@ -0,0 +1,6 @@
+//Object property deletion:
+//This syntax can be used to delete a property from an object. For example, the following code will delete the age property from the person object:
+const person = { name: "John Doe", age: 30 };
+delete person.age;
+console.log(person); // { name: "John Doe" }
+
