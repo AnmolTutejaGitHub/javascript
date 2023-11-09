408a0ff9d173a94527b4ce235306b3eb6cc03ec7
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-09 16:19:35 +0530

    Add files via upload

---
+++ javascript basics/Eval function.js
@@ -0,0 +1,6 @@
+//Eval function:
+//The eval() function evaluates a string as JavaScript code and returns the result. This can be useful for dynamically executing code, but it should be used with caution, as it can introduce security vulnerabilities. For example, the following code will evaluate the string "2 + 3" and print the result to the console:
+
+const expression = "2 + 3";
+const result = eval(expression);
+console.log(result); // 5
