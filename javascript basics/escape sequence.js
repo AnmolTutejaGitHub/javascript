bbb3ae58832ef3005023352b43acc7f8dcd8c6ee
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-11-09 16:10:35 +0530

    Add files via upload

---
+++ javascript basics/escape sequence.js
@@ -0,0 +1,4 @@
+//Unescape and escape sequences:
+//JavaScript supports unescape and escape sequences for representing special characters in strings. For example, the following code will print the copyright symbol to the console:
+
+console.log(unescape("%copyright")); // ©
