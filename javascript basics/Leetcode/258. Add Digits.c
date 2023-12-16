68d88591401d3ee3744870dbb2667db06246eeb2
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-12-17 00:12:38 +0530

    Create 258. Add Digits.c

---
+++ javascript basics/Leetcode/258. Add Digits.c
@@ -0,0 +1,12 @@
+int addDigits(int num) {
+    int sum=0;
+    while(num>0){
+         sum+=num%10;
+         num=num/10;
+         if((num<0 || num==0)&& sum>9){
+             num=sum;
+             sum=0;
+         }
+         }
+      return sum ;
+    }
