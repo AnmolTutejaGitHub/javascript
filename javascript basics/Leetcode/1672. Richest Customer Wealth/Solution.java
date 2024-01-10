09fe3b00886c602d53dc8fd1ecbe2dec5620a8b2
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-10 18:20:38 +0530

    Create Solution.java

---
+++ javascript basics/Leetcode/1672. Richest Customer Wealth/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        //int localsum=0;
+        for(int i=0;i<accounts.length;i++){
+            int localsum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                localsum+=accounts[i][j];
+            }
+            if(max<localsum) max=localsum;
+        }
+        return max;
+    }
+}
