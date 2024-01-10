493fa660f238cded76ea5c0f6b1970b339c547ef
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-10 18:56:28 +0530

    Create Solution.java

---
+++ javascript basics/Leetcode/1528. Shuffle String/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        //String ans="";
+
+        // for(int i=0;i<indices.length;i++) ans+=""+s.charAt(indices[i]);
+        // return ans ;
+
+        char [] ans = new char [indices.length];
+        for(int i=0; i<indices.length;i++) ans[indices[i]]= s.charAt(i);
+       // return String.join(ans,"");
+       return String.valueOf(ans); // valueOf method to convert char array to String 
+    }
+}
