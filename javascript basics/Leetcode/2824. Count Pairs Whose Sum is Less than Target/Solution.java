b0ed395e49e9e4061332abf894554f762e7a4355
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-11 13:29:34 +0530

    Create Solution.java

---
+++ javascript basics/Leetcode/2824. Count Pairs Whose Sum is Less than Target/Solution.java
@@ -0,0 +1,15 @@
+   public int countPairs(List<Integer> nums, int target) {
+
+        // problem faced toArray method only takes object so used Integer wrapper class
+        // instead of int
+        Integer arr[] = nums.toArray(new Integer[nums.size()]);
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] < target)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
