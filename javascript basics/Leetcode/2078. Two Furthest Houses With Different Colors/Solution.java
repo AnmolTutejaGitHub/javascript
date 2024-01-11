fbc689f4cc50a40812f39c48362636b83be5acd9
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-12 01:04:18 +0530

    Create Solution.java

---
+++ javascript basics/Leetcode/2078. Two Furthest Houses With Different Colors/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int max=0;
+        int localMax=0;
+       for(int i=0;i<colors.length;i++) {
+           localMax=0;
+           for(int j=colors.length-1;j>=0;j--){
+               if(colors[i]!=colors[j]) if(localMax<Math.abs(i-j))localMax=Math.abs(i-j);
+           }
+           if(localMax>max) max=localMax;
+       }
+       return max;
+    }
+}
