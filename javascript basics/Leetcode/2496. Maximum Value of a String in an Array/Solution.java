a30bcf5f8911e295af935ab4327c68a8046ca5fc
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-13 02:29:41 +0530

    Create Solution.java

---
+++ javascript basics/Leetcode/2496. Maximum Value of a String in an Array/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumValue(String[] strs) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<strs.length;i++){
+            String str = strs[i];
+            for(int j=0;j<str.length();j++){
+                try{
+                    if(Integer.parseInt(str)>max){
+                        max= Integer.parseInt(str);
+                    }
+                }
+                catch(NumberFormatException e){
+                    if(str.length()>max) max=str.length();
+                }
+
+            }
+        }
+        return max;
+    }
+}
